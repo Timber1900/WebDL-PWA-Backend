14f37001de6b306fbd62f988f75086f98cf85b82
Timber1900 <Timber.gamer@gmail.com>
2021-03-01 22:27:57 +0000

    No more ytdl-core

--- src/index.js
+++ src/index.js
@@ -1,23 +1,58 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const enableWs = require('express-ws')
-const cp = require('child_process');
+const { spawn } = require('child_process');
 const ffmpeg = require('ffmpeg-static');
 const ytdl = require('ytdl-core')
 const ytpl = require('ytpl')
+const YoutubeDlWrap = require('youtube-dl-wrap');
+const PassThrough = require('stream').PassThrough;
+
+
+const youtubeDlWrap = new YoutubeDlWrap("./src/youtube.exe");
+
+(async () => {
+  // await YoutubeDlWrap.downloadFromWebsite("./src/youtube.exe", "win32");
+  console.log(`Set youtube dl path: ${youtubeDlWrap}`)
+})()
+
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(cors())
-enableWs(app)
+
+app.post('/info', async (req, res) => {
+  const {url} = req.body;
+  let videos;
+    try {
+      const result = await ytpl(url, { pages: Infinity })
+      videos = result.items.map((val, i) => val.shortUrl);
+    } catch {
+      videos = [url]
+    }
+    const all = [];
+
+    for(const vid of videos) {
+      all.push(getInfo(vid))
+    }
+
+    Promise.all(all)
+    .then(val => {
+      const filtered = val.filter((val, i) => val.info)
+      res.send(JSON.stringify(filtered))
+    })
+})
 
 app.post('/video', (req, res) => {
-  const video = ytdl(req.body.url, {format: req.body.format[1]})
-  const audio = ytdl(req.body.url, {format: req.body.format[0]})
-  const ffmpegProcess = cp.spawn(ffmpeg, [
+  const video = execStream([req.body.url, '-f', 'bestvideo'])
+  const audio = execStream([req.body.url, '-f', 'bestaudio'])
+
+  video.on('close', () => video.destroy())
+  audio.on('close', () => audio.destroy())
+
+  const ffmpegProcess = spawn(ffmpeg, [
     '-loglevel', '8', '-hide_banner',
     '-i', 'pipe:4',
     '-i', 'pipe:5',
@@ -25,6 +60,7 @@ app.post('/video', (req, res) => {
     '-map', '1:v',
     '-c:v', 'copy',
     '-f', 'matroska',
+    '-metadata', 'major_brand=\"mp42\"',
     'pipe:3',
   ], {
     windowsHide: true,
@@ -47,44 +83,29 @@ app.post('/video', (req, res) => {
 const getInfo = (url) => {
   return new Promise(async (res, rej) => {
     try {
-      const info = await ytdl.getInfo(url);
-      const highestvideo = await ytdl.chooseFormat(info.formats, {quality: 'highestvideo'})
-      const highestaudio = await ytdl.chooseFormat(info.formats, {quality: 'highestaudio'})
-      res({info, formats: [highestaudio, highestvideo]})
+      const info = await youtubeDlWrap.getVideoInfo(url);
+      res({ info })
     } catch (error) {
       res({})
     }
   })
 }
 
-app.ws('/info', (socket, req) => {
-  socket.on('message', async (msg) => {
-    console.log(msg)
-    let videos;
-      try {
-        const result = await ytpl(msg, { pages: Infinity })
-        videos = result.items.map((val, i) => val.shortUrl);
-      } catch {
-        videos = [msg]
-      }
-      console.log(videos)
-      const all = [];
+const execStream = (youtubeDlArguments = []) => {
+  youtubeDlArguments = youtubeDlArguments.concat(["-o", "-"]);
+  const youtubeDlProcess = spawn(youtubeDlWrap.binaryPath, youtubeDlArguments, {
+    windowsHide: true,
+    maxBuffer: 1024 * 1024 * 1024
+  });
 
-      for(const vid of videos) {
-        all.push(getInfo(vid))
-      }
-
-      Promise.all(all)
-      .then(val => {
-        const filtered = val.filter((val, i) => val.info)
-        console.log(filtered)
-        socket.send(JSON.stringify(filtered))
-      })
-    })
-
-  socket.on('close', () => {
-    console.log('WebSocket was closed')
-  })
-})
+  let stderrData = "";
+  youtubeDlProcess.stderr.on("data", (data) =>
+  {
+    let stringData = data.toString();
+    stderrData += stringData
+  });
+  youtubeDlProcess.on("error", (error) => console.log(`Youtube DL exited with code ${error}, heres some error data: ${stderrData}`));
+  return youtubeDlProcess.stdout;
+}
 
 app.listen(8080, () => {console.log(`Server open on port 8080`)})
