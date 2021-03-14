9d6dc065311fff19b951589032e69e54e8976bce
Timber1900 <Timber.gamer@gmail.com>
2021-03-14 01:17:53 +0000

    Fmmpeg is now on frontend

--- src/index.js
+++ src/index.js
@@ -3,16 +3,17 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const { spawn } = require('child_process');
 const ffmpeg = require('ffmpeg-static');
-const ytdl = require('ytdl-core')
-const ytpl = require('ytpl')
 const YoutubeDlWrap = require('youtube-dl-wrap');
-const PassThrough = require('stream').PassThrough;
+const fs = require('fs')
 
-
-const youtubeDlWrap = new YoutubeDlWrap("./src/youtube.exe");
+const youtubeDlWrap = new YoutubeDlWrap("./src/youtube");
 
 (async () => {
-  // await YoutubeDlWrap.downloadFromWebsite("./src/youtube.exe", "win32");
+  await YoutubeDlWrap.downloadFromWebsite("./src/youtube", "linux");
+  fs.chmod("./src/youtube", fs.constants.S_IXUSR | fs.constants.S_IRUSR | fs.constants.S_IWUSR, (error) => {
+    if(error) console.error(error)
+    console.log('Changed file permissions');
+  });
   console.log(`Set youtube dl path: ${youtubeDlWrap}`)
 })()
 
@@ -25,71 +26,67 @@ app.use(cors())
 
 app.post('/info', async (req, res) => {
   const {url} = req.body;
-  let videos;
-    try {
-      const result = await ytpl(url, { pages: Infinity })
-      videos = result.items.map((val, i) => val.shortUrl);
-    } catch {
-      videos = [url]
-    }
-    const all = [];
 
-    for(const vid of videos) {
-      all.push(getInfo(vid))
-    }
-
-    Promise.all(all)
-    .then(val => {
-      const filtered = val.filter((val, i) => val.info)
-      res.send(JSON.stringify(filtered))
-    })
+  let final = "["
+  const proc = spawn(youtubeDlWrap.binaryPath, [url, '-i', '--dump-json', '--restrict-filenames'], {})
+  proc.stderr.on('data', (val) => {console.log('\x1b[31m%s\x1b[0m', val.toString())})
+  proc.stdout.on('data', (val) => {final += val.toString()})
+  proc.on('close', () => {
+    let finalString = final.replace(/(?:\r\n|\r|\n)/g, ",").substring(0, final.length - 1);
+    // let newFinal = final.substring(0, final.length - 2);
+	  finalString += "]";
+    res.send(finalString);
+	  console.log('close');
+	})
 })
 
 app.post('/video', (req, res) => {
   const video = execStream([req.body.url, '-f', 'bestvideo'])
-  const audio = execStream([req.body.url, '-f', 'bestaudio'])
-
-  video.on('close', () => video.destroy())
-  audio.on('close', () => audio.destroy())
-
-  const ffmpegProcess = spawn(ffmpeg, [
-    '-loglevel', '8', '-hide_banner',
-    '-i', 'pipe:4',
-    '-i', 'pipe:5',
-    '-map', '0:a',
-    '-map', '1:v',
-    '-c:v', 'copy',
-    '-f', 'matroska',
-    '-metadata', 'major_brand=\"mp42\"',
-    'pipe:3',
-  ], {
-    windowsHide: true,
-    stdio: [
-      'inherit', 'inherit', 'inherit',
-      'pipe', 'pipe', 'pipe',
-    ],
-  });
-
-  audio.pipe(ffmpegProcess.stdio[4]);
-  video.pipe(ffmpegProcess.stdio[5]);
-
-  ffmpegProcess.stdio[3].on('close', () => {
+  // const audio = execStream([req.body.url, '-f', 'bestaudio'])
+  video.on('close', () => {
     console.log('done')
+    video.destroy()
     res.end()
-  });
-  ffmpegProcess.stdio[3].pipe(res);
+  })
+  video.pipe(res);
+  // audio.on('close', () => audio.destroy())
+
+  // const ffmpegProcess = spawn(ffmpeg, [
+  //   '-loglevel', '8', '-hide_banner',
+  //   '-i', 'pipe:4',
+  //   '-i', 'pipe:5',
+  //   '-map', '0:a',
+  //   '-map', '1:v',
+  //   '-c:v', 'copy',
+  //   '-f', 'matroska',
+  //   'pipe:3',
+  // ], {
+  //   windowsHide: true,
+  //   stdio: [
+  //     'inherit', 'inherit', 'inherit',
+  //     'pipe', 'pipe', 'pipe',
+  //   ],
+  // });
+
+  // audio.pipe(ffmpegProcess.stdio[4]);
+  // video.pipe(ffmpegProcess.stdio[5]);
+
+  // ffmpegProcess.stdio[3].on('close', () => {
+  //   console.log('done')
+  //   res.end()
+  // });
+  // ffmpegProcess.stdio[3].pipe(res);
 })
 
-const getInfo = (url) => {
-  return new Promise(async (res, rej) => {
-    try {
-      const info = await youtubeDlWrap.getVideoInfo(url);
-      res({ info })
-    } catch (error) {
-      res({})
-    }
+app.post('/audio', (req, res) => {
+  const audio = execStream([req.body.url, '-f', 'bestaudio'])
+  audio.on('close', () => {
+    audio.destroy();
+    res.end();
   })
-}
+  audio.pipe(res);
+})
+
 
 const execStream = (youtubeDlArguments = []) => {
   youtubeDlArguments = youtubeDlArguments.concat(["-o", "-"]);
@@ -108,4 +105,10 @@ const execStream = (youtubeDlArguments = []) => {
   return youtubeDlProcess.stdout;
 }
 
-app.listen(8080, () => {console.log(`Server open on port 8080`)})
+app.get('/', (req, res) => {
+  res.json({message: 'Welcome to the WebDL Api! 🎈🎈'})
+})
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {console.log(`Server open on port 8080`)})
